18dc69534303ff986a341393103a0bdf91063050
vikram1110dev <vikrambalamurugan11@gmail.com>
2026-08-05 21:56:38 +0530

    feat: Add BackToTopButton component skeleton

---
+++ frontend/src/components/BackToTopButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BackToTopButton: React.FC = () => {
+    return (
+        <button>
+            Back to Top
+        </button>
+    );
+};
+
+export default BackToTopButton;
